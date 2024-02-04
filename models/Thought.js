87c5cf70fb0d3c21c211042249556ade0c22395a
Vanessa Trinh <xtourmaline@gmail.com>
2024-02-04 01:40:32 -0500

    finished thought.js

---
+++ models/Thought.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require("./Reaction");
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: [1, "Too short"],
+            maxlength: [280, "Too long"]
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: () => {
+                return this.createdAt.toLocaleDateString("en-US");
+            }
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    }
+);
+
+thoughtSchema.virtual("reactionCount").get(() => {
+    return this.reactions.length;
+})
+
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
