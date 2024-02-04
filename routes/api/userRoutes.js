3abedf4d269c2f1bbbd2c4aa43e4cc52f06afaaf
Vanessa Trinh <xtourmaline@gmail.com>
2024-02-04 02:11:04 -0500

    thought routes completed

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -17,6 +17,6 @@ router.route('/').get(getUsers).post(createUser);
 router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 
 // /api/users/:userId/friends/:friendId
-router.route('/api/users/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
 
 module.exports = router;
